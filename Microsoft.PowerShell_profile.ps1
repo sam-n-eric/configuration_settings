b13c6d5d451dd34da81d2e5a630822b1559cdbaf
sam-n-eric <sam_n_eric@hotmail.com>
2018-02-28 18:04:51 -0600

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,5 @@ $profileScript = (Split-Path -Path $profile) + "\a_brit_abroad_profile.ps1"
 
 Set-Alias -Name BH -Value BranchHistory
 Set-Alias -Name BD -Value BranchDiff
+
+function ngm { Invoke-Expression 'npm run mock' }
